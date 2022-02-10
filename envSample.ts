66c4bde84fe96d01c22a4c2e4fee726af7b44605
John Li (Tet) <tet@tet.moe>
2022-02-09 21:51:46 -0800

    added jwt secret

--- envSample.ts
+++ envSample.ts
@@ -1,5 +1,8 @@
+import { readFileSync } from "fs";
+
 export const env = {
   port: 443, // <-- default port to listen
   mongo: "", // <-- put your mongo connection URL here
+  jwtSecret: readFileSync("./keys/private.key", "utf8"), // <-- put your JWT secret here
 };
 export default env;
