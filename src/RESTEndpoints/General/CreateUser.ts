dc3aac340c3d756512cd8e8761b9edd614d78879
John Li (Tet) <tet@tet.moe>
2022-02-11 10:34:19 -0800

    edit change user to add password

--- src/RESTEndpoints/General/CreateUser.ts
+++ src/RESTEndpoints/General/CreateUser.ts
@@ -24,9 +24,25 @@ export const GetGame = {
     /*
     {"_id":{"$oid":"61ff707133c07a29b7346230"},"userID":"TET00","username":"teto","firstName":"John","lastName":"Li","email":"tet@tet.moe","createdBy":"Disadus","admin":true,"pfp":"https://disadus.app/logo.png"}
     */
-    const { userID, username, firstName, lastName, email, admin, pfp } =
-      req.body;
-    const fields = [userID, username, firstName, lastName, email, pfp];
+    const {
+      userID,
+      username,
+      firstName,
+      lastName,
+      email,
+      admin,
+      pfp,
+      password,
+    } = req.body;
+    const fields = [
+      userID,
+      username,
+      firstName,
+      lastName,
+      email,
+      pfp,
+      password,
+    ];
     const missingFields = fields.filter((field) => !field);
     if (missingFields.length > 0) {
       return res
@@ -46,6 +62,7 @@ export const GetGame = {
       admin,
       pfp,
       createdBy: appID,
+      password,
     } as User;
     res.status(200).json(await createUser(newUser));
   },
