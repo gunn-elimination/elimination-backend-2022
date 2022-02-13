77a9856e8a945e44e0847d5471615b10cc78154e
John Li (Tet) <tet@tet.moe>
2022-02-12 18:29:10 -0800

    fixed verify signup

--- src/RESTEndpoints/General/VerifySignup.ts
+++ src/RESTEndpoints/General/VerifySignup.ts
@@ -7,6 +7,10 @@ import {
 } from "../../Helpers/UserAPIs";
 import nodemailer from "nodemailer";
 import { env } from "../../../env";
+import { Encryptions } from "../../Helpers/Encryptions";
+interface UnverifiedUser extends User {
+  verificationNonce?: string;
+}
 export const GetGame = {
   path: "/verify/:verificationNonce",
   method: RESTMethods.POST,
@@ -16,19 +20,21 @@ export const GetGame = {
     if (!nonce) {
       return res.status(400).redirect("Bad Request");
     }
-    const user = await MongoDB.db("UserData")
+    const user = (await MongoDB.db("UserData")
       .collection("unverifiedUsers")
       .findOne({
         verificationNonce: nonce,
-      });
+      })) as unknown as UnverifiedUser;
     if (!user) {
       return res.status(404).send("User not found");
     }
     delete user.verificationNonce;
-
+    user.userID = user.email.split("@")[0].toUpperCase();
     const response = await createUser(user as unknown as User);
     if (response.success) {
-      return res.status(200).send(user);
+      return res
+        .status(200)
+        .send(await Encryptions.issueUserToken(user.userID));
     }
     return res.status(500).send(response.message);
   },
