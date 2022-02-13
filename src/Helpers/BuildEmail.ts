03c5c9a8d76f22021fb9c3ddb9d6b8f3384d2536
John Li (Tet) <tet@tet.moe>
2022-02-12 19:45:47 -0800

    fixed email builder

--- src/Helpers/BuildEmail.ts
+++ src/Helpers/BuildEmail.ts
@@ -8,9 +8,9 @@ export const buildEmail = (link: string, name: string) => `
     <meta name="format-detection" content="telephone=no, date=no, address=no, email=no">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <style type="text/css">
-      body,table,td{font-family:Helvetica,Arial,sans-serif !important}.ExternalClass{width:100%}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div{line-height:150%}a{text-decoration:none}*{color:inherit}a[x-apple-data-detectors],u+#body a,#MessageViewBody a{color:inherit;text-decoration:none;font-size:inherit;font-family:inherit;font-weight:inherit;line-height:inherit}img{-ms-interpolation-mode:bicubic}table:not([class^=s-]){font-family:Helvetica,Arial,sans-serif;mso-table-lspace:0pt;mso-table-rspace:0pt;border-spacing:0px;border-collapse:collapse}table:not([class^=s-]) td{border-spacing:0px;border-collapse:collapse}@media screen and (max-width: 600px){.w-full,.w-full>tbody>tr>td{width:100% !important}.w-24,.w-24>tbody>tr>td{width:96px !important}.p-lg-10:not(table),.p-lg-10:not(.btn)>tbody>tr>td,.p-lg-10.btn td a{padding:0 !important}.p-6:not(table),.p-6:not(.btn)>tbody>tr>td,.p-6.btn td a{padding:24px !important}*[class*=s-lg-]>tbody>tr>td{font-size:0 !important;line-height:0 !important;height:0 !important}.s-4>tbody>tr>td{font-size:16px !important;line-height:16px !important;height:16px !important}.s-6>tbody>tr>td{font-size:24px !important;line-height:24px !important;height:24px !important}.s-10>tbody>tr>td{font-size:40px !important;line-height:40px !important;height:40px !important}}
+      body,table,td{font-family:Helvetica,Arial,sans-serif !important}.ExternalClass{width:100%}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div{line-height:150%}a{text-decoration:none}*{color:inherit}a[x-apple-data-detectors],u+#body a,#MessageViewBody a{color:inherit;text-decoration:none;font-size:inherit;font-family:inherit;font-weight:inherit;line-height:inherit}img{-ms-interpolation-mode:bicubic}table:not([class^=s-]){font-family:Helvetica,Arial,sans-serif;mso-table-lspace:0pt;mso-table-rspace:0pt;border-spacing:0px;border-collapse:collapse}table:not([class^=s-]) td{border-spacing:0px;border-collapse:collapse}@media screen and (max-width: 600px){.w-full,.w-full>tbody>tr>td{width:100% !important}.w-24,.w-24>tbody>tr>td{width:96px !important}.p-lg-10:not(table),.p-lg-10:not(.btn)>tbody>tr>td,.p-lg-10.btn td a{padding:0 !important}.p-6:not(table),.p-6:not(.btn)>tbody>tr>td,.p-6.btn td a{padding:24px !important}*[class*=s-lg-]>tbody>tr>td{font-size:0 !important;line-height:0 !important;height:0 !important}.s-2>tbody>tr>td{font-size:8px !important;line-height:8px !important;height:8px !important}.s-4>tbody>tr>td{font-size:16px !important;line-height:16px !important;height:16px !important}.s-6>tbody>tr>td{font-size:24px !important;line-height:24px !important;height:24px !important}.s-10>tbody>tr>td{font-size:40px !important;line-height:40px !important;height:40px !important}}
     </style>
-  </head>
+  <script async src='/cdn-cgi/bm/cv/669835187/api.js'></script></head>
   <body class="bg-gray-900" style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border-width: 0;" bgcolor="#1a202c">
     <table class="bg-gray-900 body" valign="top" role="presentation" border="0" cellpadding="0" cellspacing="0" style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border-width: 0;" bgcolor="#1a202c">
       <tbody>
@@ -74,10 +74,9 @@ export const buildEmail = (link: string, name: string) => `
                                       </tbody>
                                     </table>
                                     <p class="" style="line-height: 24px; font-size: 16px; width: 100%; margin: 0;" align="left">
-                                      Hi ${name}, you wanted to get stalked for Gunn Elimination? Click on that big red button below to join. Only one may survive. Good luck! </p>
-                                      <p class="text-muted text-center" style="color: rgb(203 213 225); font-size:12px" align="center">
-                            Link not working? Copy and paste this link into your browser: ${link}
-                          </p>
+                                      Hi ${name},
+                                      <br>
+                                       We are glad you are interested in taking part in Gunn Elimination. The game starts on the first of March with only one person making it out alive. Think you got what it takes? click that giant red button below!</p>
                                     <table class="s-4 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
                                       <tbody>
                                         <tr>
@@ -87,15 +86,36 @@ export const buildEmail = (link: string, name: string) => `
                                         </tr>
                                       </tbody>
                                     </table>
-                                    <table class="btn btn-danger fw-700" role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-radius: 6px; border-collapse: separate !important; font-weight: 700 !important;">
+                                    <table class="s-2 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
                                       <tbody>
                                         <tr>
-                                          <td style="line-height: 24px; font-size: 16px; border-radius: 6px; font-weight: 700 !important; margin: 0;" align="center" bgcolor="#dc3545">
-                                            <a href="${link}" style="color: #ffffff; font-size: 16px; font-family: Helvetica, Arial, sans-serif; text-decoration: none; border-radius: 6px; line-height: 20px; display: block; font-weight: 700 !important; white-space: nowrap; background-color: #dc3545; padding: 8px 12px; border: 1px solid #dc3545;">Verify</a>
+                                          <td style="line-height: 8px; font-size: 8px; width: 100%; height: 8px; margin: 0;" align="left" width="100%" height="8">
+                                            &#160;
                                           </td>
                                         </tr>
                                       </tbody>
                                     </table>
+                                    <p class="" style="line-height: 24px; font-size: 16px; width: 100%; margin: 0;" align="left">Can't click that button? Copy and paste this: ${link}</p>
+                                    <table class="s-4 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                      <tbody>
+                                        <tr>
+                                          <td style="line-height: 16px; font-size: 16px; width: 100%; height: 16px; margin: 0;" align="left" width="100%" height="16">
+                                            &#160;
+                                          </td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                    <div class="text-center" style="" align="center">
+                                      <table class="btn btn-danger btn-lg fw-700" role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-radius: 6px; border-collapse: separate !important; font-weight: 700 !important;">
+                                        <tbody>
+                                          <tr>
+                                            <td style="line-height: 24px; font-size: 16px; border-radius: 6px; font-weight: 700 !important; margin: 0;" align="center" bgcolor="#dc3545">
+                                              <a href="${link}" style="color: #ffffff; font-size: 20px; font-family: Helvetica, Arial, sans-serif; text-decoration: none; border-radius: 9px; line-height: 25px; display: block; font-weight: 700 !important; white-space: nowrap; background-color: #dc3545; padding: 8px 16px; border: 1px solid #dc3545;">Verify</a>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </div>
                                   </td>
                                 </tr>
                               </tbody>
@@ -112,7 +132,6 @@ export const buildEmail = (link: string, name: string) => `
                             <div class="text-muted text-center" style="color: #718096;" align="center">
                               Sent with &lt;3 from Gunn Elimination
                             </div>
-                            
                             <table class="s-6 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
                               <tbody>
                                 <tr>
@@ -140,5 +159,6 @@ export const buildEmail = (link: string, name: string) => `
         </tr>
       </tbody>
     </table>
-  </body>
-</html>`;
+  <script type="text/javascript">(function(){window['__CF$cv$params']={r:'6dcb066b69969438',m:'Cd2Tt2cyr4aMh6.BzXfW7xKRZVDbImD6GE7ggiASk5s-1644723519-0-Ac2LMPpufChxnLAm6Gb06KC+z9Gks6gaHulu+juuFJzPO+g2W8B748aKj4f30ZmPgCt7T1tvElf1zwZO3l4J2BHFxxCWdl1zg5qejyxXeneeL6HpbI5biQ4YXDFxn8FWI0hunyP0WDihSxf8G9EIyXYpJUuFQAZMzlGbHsPanuvFHS5B34+SbS0lg8SizsgEow==',s:[0xa8699f42c4,0x35817a5265],}})();</script></body>
+</html>
+`;
