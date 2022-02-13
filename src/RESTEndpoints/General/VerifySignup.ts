2933d9744bfe8d4bf60983861cbf61dd7c0ed4dc
John Li (Tet) <tet@tet.moe>
2022-02-12 19:09:07 -0800

    possibly catch error?

--- src/RESTEndpoints/General/VerifySignup.ts
+++ src/RESTEndpoints/General/VerifySignup.ts
@@ -40,9 +40,12 @@ export const GetGame = {
         .findOneAndDelete({
           verificationNonce: nonce,
         });
-      const token = await Encryptions.issueUserToken(user.userID);
+      const token = await Encryptions.issueUserToken(user.userID).catch(er=>{
+        console.log(er);
+      });
       console.log(token);
-      return res.redirect(`${redirectURL}/login?token=${token}`);
+      res.redirect(`${redirectURL}/login?token=${token}`);
+      return;
     }
     return res.status(500).send(response.message);
   },
