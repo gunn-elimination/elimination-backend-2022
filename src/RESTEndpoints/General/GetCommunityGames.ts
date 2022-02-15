c0eb72227100721eb0f39ba454357ea936c4e200
John Li (Tet) <tet@tet.moe>
2022-02-14 22:21:39 -0800

    forgot to remove communityID requirement

--- src/RESTEndpoints/General/GetCommunityGames.ts
+++ src/RESTEndpoints/General/GetCommunityGames.ts
@@ -5,11 +5,7 @@ export const getGamesAPI = {
   path: "/games",
   method: RESTMethods.GET,
   sendUser: false,
-  run: async (req, res, next, user) => {
-    const communityID = req.params.communityID;
-    if (!communityID) {
-      return res.status(400).send("Bad Request");
-    }
+  run: async (req, res, next, _) => {
     res.status(200).send(await getGames());
   },
 } as RESTHandler;
