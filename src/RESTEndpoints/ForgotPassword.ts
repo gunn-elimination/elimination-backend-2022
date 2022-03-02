a9ca0bf2b47af389222d133cfadc82793c8809ba
John Li (Tet) <tet@tet.moe>
2022-03-01 22:00:23 -0800

    added password EPs

---
+++ src/RESTEndpoints/ForgotPassword.ts
@@ -0,0 +1,61 @@
+import { env } from "../../env";
+import { RESTMethods, RESTHandler, User } from "../../types/DisadusTypes";
+import { buildEmail } from "../Helpers/BuildEmail";
+import TetLib from "../Helpers/TetLib";
+import { getUserByEmail } from "../Helpers/UserAPIs";
+import nodemailer from "nodemailer";
+export const Signup = {
+  path: "/signup",
+  method: RESTMethods.POST,
+  sendUser: false,
+  run: async (req, res, next, _) => {
+    const { email, redirectURL } = req.body;
+    if (!email || !redirectURL) {
+      return res.status(400).send(`Missing email or redirectURL`);
+    }
+    const unverifiedUserExists = await MongoDB.db("UserData")
+      .collection("resetPasswords")
+      .findOne({
+        email,
+      });
+    if (unverifiedUserExists) {
+      return res.status(409).send("Email already exists");
+    }
+    const verificationNonce = TetLib.genID(128);
+    const emailExists = (await getUserByEmail(email)) as any as User;
+    if (!emailExists) {
+      return res.status(409).send("Email does not exist");
+    }
+    const emailData = {
+      email,
+      userID: emailExists.userID,
+      redirectURL,
+      verificationNonce,
+    };
+    await MongoDB.db("UserData")
+      .collection("resetPasswords")
+      .insertOne(emailData);
+    await nodemailer
+      .createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: env.email.email,
+          pass: env.email.password,
+        },
+      })
+      .sendMail({
+        from: `Gunn Elimination <support@gunnelimination.com>`,
+        to: emailExists.email,
+        subject: "Reset your password",
+        text: `
+            Hello ${emailExists.firstName} ${emailExists.lastName},
+            Please reset your password by clicking the link below:
+            ${redirectURL}?token=${verificationNonce}
+          `,
+      });
+    res.status(200).send("OK");
+  },
+} as RESTHandler;
+export default Signup;
---
+++ src/RESTEndpoints/SetPassword.ts
@@ -0,0 +1,40 @@
+import { RESTMethods, RESTHandler, User } from "../../types/DisadusTypes";
+import phas from "password-hash-and-salt";
+interface UnverifiedUser extends User {
+  verificationNonce?: string;
+  redirectURL?: string;
+}
+export const GetGame = {
+  path: "/set-password",
+  method: RESTMethods.POST,
+  sendUser: false,
+  run: async (req, res, next, _) => {
+    const { password, token } = req.body;
+    if (!password || !token) {
+      return res.status(400).redirect("Bad Request");
+    }
+    const user = (await MongoDB.db("UserData")
+      .collection("resetPasswords")
+      .findOne({
+        verificationNonce: token,
+      })) as unknown as UnverifiedUser;
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    const newpass = await new Promise((res, rej) =>
+      phas(user.password).hash((err, hash) => {
+        if (err) rej(err);
+        res(hash);
+      })
+    );
+    await MongoDB.db("UserData")
+      .collection("users")
+      .updateOne({ userID: user.userID }, { $set: { password: newpass } });
+    await MongoDB.db("UserData").collection("resetPasswords").findOneAndDelete({
+      verificationNonce: token,
+    });
+    res.status(200).send("Password changed");
+    return;
+  },
+} as RESTHandler;
+export default GetGame;
