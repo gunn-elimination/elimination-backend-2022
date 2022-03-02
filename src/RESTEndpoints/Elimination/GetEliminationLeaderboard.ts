ce62a756bc4d6dc4df713e90674d140c9832a6cf
John Li (Tet) <tet@tet.moe>
2022-03-01 21:34:02 -0800

    expanding leaderboard to 150auto 500min ppl :flushwobble

--- src/RESTEndpoints/Elimination/GetEliminationLeaderboard.ts
+++ src/RESTEndpoints/Elimination/GetEliminationLeaderboard.ts
@@ -6,7 +6,7 @@ export const GetEliminatioonLeaderboard = {
   sendUser: true,
   run: async (req, res, next, user) => {
     const gameID = req.params.gameID;
-    const limit = Math.min(Number(req.query.limit) || 75, 75);
+    const limit = Math.min(Number(req.query.limit) || 150, 150);
 
     if (!gameID || !user) {
       return res.status(400).send("Bad Request");
